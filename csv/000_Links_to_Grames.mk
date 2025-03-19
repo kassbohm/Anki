36b25860a294fec16e4dac73b88c02153d6d2ce2
kassbohm <kassbohm@gmail.com>
2025-03-20 06:42:53 +0700

    "tag"

---
+++ csv/000_Links_to_Grames.mk
@@ -0,0 +1,30 @@
+# Links to Games
+
+## Zahlen
+
+- [A](https://dashboard.blooket.com/set/67b6ab9359ae22c981ad8277)
+- [B](https://dashboard.blooket.com/set/67b6b17e59ae22c981ad82db)
+- [C](https://dashboard.blooket.com/set/67b6b382498073edbf496798)
+- [D](https://dashboard.blooket.com/set/67b6b52659ae22c981ad8315)
+
+
+## Substantiv
+
+- [01](https://dashboard.blooket.com/set/67d3c88f707b8daeefbc441f)
+- [02 A](https://dashboard.blooket.com/set/67d7e2bcadbdf31fb8e424af)
+- [02 B](https://dashboard.blooket.com/set/67d7e1dab1f911dfb1dcf70c)
+- [03](https://dashboard.blooket.com/set/67d91ac5889dd142d8b5b723)
+
+## Verb
+
+- [01](https://dashboard.blooket.com/set/67d634d936eec4198008ac4d)
+- [02](https://dashboard.blooket.com/set/67d81072ba61e9c90f195798)
+- [trennbar](https://dashboard.blooket.com/set/67da60d0b12af322bd4a200d)
+- [unregelmäßig](https://dashboard.blooket.com/set/67dab28eb12af322bd4a24a3)
+
+## Datum und Uhrzeit
+
+- [Datum C](https://dashboard.blooket.com/set/67d7b6d2b1f911dfb1dcf482)
+- [Datum D](https://dashboard.blooket.com/set/67d7b891b1f911dfb1dcf4a9)
+- [Uhrzeit](https://dashboard.blooket.com/set/67c591b26472c7b14921e6aa)
+
